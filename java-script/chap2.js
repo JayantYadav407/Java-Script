f527a14f5ff19592d8b181fba3d190fa0441b43d
Jayant yadav <mjvibes12am@gmail.com>
2026-02-04 07:57:38 +0000

    data-type1

---
+++ java-script/chap2.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert("hello") we are using node js
+console.log("Hello Duniya")
+
+let isloggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof(""));
+// null is object 
+// null and null is undefined 
